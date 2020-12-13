c87f4316b8167fc594d2f76d44eb652af9a29fad
Yeasin Abedin <yabe@stormgeo.com>
2020-12-14 00:10:49 +0600

    Generate jwt token and send to client with dummy username(1234), password(1234)

---
+++ src/jwt-authentication-server/JwtAuthenticationServer/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using JwtAuthenticationServer.Models;
+using JwtAuthenticationServer.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthenticationServer.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager _jwtAuthManager;
+        public AuthController(IJwtAuthenticationManager jwtAuthManager)
+        {
+            _jwtAuthManager = jwtAuthManager;
+        }
+
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody]LoginModel user)
+        {
+            if (user == null)
+                return BadRequest("Invalid client request");
+
+            // dummy username and password
+            if(user.Username == "1234" && user.Password == "1234")
+            {
+                var jwtToken = _jwtAuthManager.GenerateJwtToken();
+                return Ok(jwtToken);
+            }
+
+            return Unauthorized();
+        }
+    }
+}
