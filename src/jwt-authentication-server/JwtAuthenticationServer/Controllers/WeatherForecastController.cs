25ef0e5ffe51e95635b5372f8caf478ef6bf49fe
Yeasin Abedin <yabe@stormgeo.com>
2020-12-14 00:30:05 +0600

    Login and get a token. Use that token to access Authorized WeatherForecastController. Check token expiration after 1min

--- src/jwt-authentication-server/JwtAuthenticationServer/Controllers/WeatherForecastController.cs
+++ src/jwt-authentication-server/JwtAuthenticationServer/Controllers/WeatherForecastController.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace JwtAuthenticationServer.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
